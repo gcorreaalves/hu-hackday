58345aaf424303998ecf84bcee42832b1a74fed7
Vitor Bittencourt <vitorhp2@gmail.com>
2015-07-31 02:11:47 -0300

    Added locale pt-BR

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,8 +2,8 @@ class CollectionsController < ApplicationController
   def show
     @items = Media.where(city_slug: params[:id]).each_with_object({}) do |item, memo|
       memo[item.created_at_ig.year] ||= {}
-      memo[item.created_at_ig.year][item.created_at_ig.strftime('%B')] ||= []
-      memo[item.created_at_ig.year][item.created_at_ig.strftime('%B')] << item
+      memo[item.created_at_ig.year][I18n.l(item.created_at_ig, format: :month)] ||= []
+      memo[item.created_at_ig.year][I18n.l(item.created_at_ig, format: :month)] << item
     end
   end
 end
