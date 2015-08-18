8b18cd76ce09897c0657d5a2628da80a8784f36b
Vitor Bittencourt <vitorhp2@gmail.com>
2015-08-18 14:43:27 -0300

    Changed hashtag to #fotografohu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @recent_huhackday = FilterValidMedia.from(client.tag_recent_media('huhackday'))
+    @recent_huhackday = FilterValidMedia.from(client.tag_recent_media('fotografohu'))
   end
 
   def landscape
